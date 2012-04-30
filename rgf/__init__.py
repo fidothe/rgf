cd3e9952a0b2a70ef9e485f7fd9fd6fafde0b00c
Matt Patterson <matt@reprocessed.org>
2012-04-30 14:58:54 +0200

    Run setup code before the example is executed.

--- rgf/__init__.py
+++ rgf/__init__.py
@@ -1,7 +1,9 @@
 class Example(object):
-    def __init__(self, description, spec_function):
+    def __init__(self, description, spec_function, example_group):
         self.spec_function = spec_function
         self.description = description
+        self.example_group = example_group
 
     def run(self):
+        self.example_group.run_before_each(self)
         self.spec_function(self)
--- tests.py
+++ tests.py
@@ -1,9 +1,20 @@
 from rgf import Example
 
+class MockExampleGroup(object):
+    def run_before_each(self, example):
+        def before(eg):
+            eg.before_was_run = True
+        before(example)
+
 # Example can be run
 def first_test_function(self):
     self.has_been_run = True
 
-first_example = Example("can be run", first_test_function)
+first_example = Example("can be run", first_test_function, MockExampleGroup())
 first_example.run()
 assert first_example.has_been_run
+
+# Example is run with before func from context
+second_example = Example("runs before method from context", first_test_function, MockExampleGroup())
+second_example.run()
+assert second_example.before_was_run
