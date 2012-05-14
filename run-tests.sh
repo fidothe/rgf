d5666b10e04f6485c4881c7fd9d861f61875824a
Matt Patterson <matt@reprocessed.org>
2012-05-14 22:17:24 +0200

    Simplify `./rgf/cold_runner.py` and move it to `./cold_runner` so that it does almost nothing except call the same function as the setuptools console_scripts entry point (which is good, given that it's standing in for setuptools/distribute's generated scripts which clearly do something, but the only thing you can really say about them is that they call a function and return its result as their exit status)

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python rgf/cold_runner.py spec
+./cold_runner spec
--- spec/rgf/core/runner/runner_spec.py
+++ spec/rgf/core/runner/runner_spec.py
@@ -24,10 +24,18 @@ with describe('Runner'):
         assert len(reporter.examples_ran) > 0
 
 with describe('rgf script'):
-    @it('can create and run a runner')
-    def f(w):
-        p = subprocess.Popen('python rgf/cold_runner.py fixture_specs/success', shell = True, stdout = subprocess.PIPE)
+    def run_spec_script(spec_path):
+        p = subprocess.Popen('./cold_runner %s' % spec_path, shell = True, stdout = subprocess.PIPE)
         output, p_null = p.communicate()
         return_val = p.wait()
-        assert return_val == 0
+        return (return_val, output)
+
+    @it('can create and run a runner')
+    def f(w):
+        return_val, output = run_spec_script('fixture_specs/success')
         assert re.compile(r'4 examples').search(output) is not None
+
+    @it('returns exit status 0 on a successful run')
+    def f(w):
+        return_val, output = run_spec_script('fixture_specs/success')
+        assert return_val == 0
