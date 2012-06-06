e761106b0888f3651157ab3bc773152c3fc5e358
Matt Patterson <matt@reprocessed.org>
2012-06-06 12:19:34 +0200

    Bring spec function naming (and nesting) into line

--- spec/rgf/core/runner/progress_formatter_spec.py
+++ spec/rgf/core/runner/progress_formatter_spec.py
@@ -1,4 +1,4 @@
-from rgf.dsl import describe, it, before
+from rgf.dsl import *
 
 try:
     from StringIO import StringIO
@@ -18,58 +18,58 @@ class MockExample(object):
 def mock_world_factory():
     pass
 
-with describe('ProgressFormatter'):
+with subject('ProgressFormatter'):
     @it('can log a success to an IO-like object')
-    def spec(world):
+    def s(w):
         io = StringIO()
         pf = ProgressFormatter(io)
         pf.success(MockExample(), (1, None))
         assert io.getvalue() == '.'
 
     @it('can log a failure to an IO-like object')
-    def spec(world):
+    def s(w):
         io = StringIO()
         pf = ProgressFormatter(io)
         pf.failure(MockExample(), (2, None))
         assert io.getvalue() == 'F'
 
     @it('can log an error to an IO-like object')
-    def spec(world):
+    def s(w):
         io = StringIO()
         pf = ProgressFormatter(io)
         pf.error(MockExample(), (3, None))
         assert io.getvalue() == 'E'
 
     @it('can summarise the results of running all examples')
-    def spec(world):
+    def s(w):
         io = StringIO()
         pf = ProgressFormatter(io)
         pf.summarise_results(total = 3, successes = 1, failures = 1, errors = 1)
         assert io.getvalue() == 'Ran 3 examples: 1 success, 1 failure, 1 error\n'
 
-with describe('ProgressFormatter summarising Failures and Errors'):
-    @before
-    def b(world):
-        def failed_test_function(self):
-            assert False
-        world.failing_example = Example("reports failure", failed_test_function)
+    with context('summarising Failures and Errors'):
+        @before
+        def b(w):
+            def failed_test_function(self):
+                assert False
+            w.failing_example = Example("reports failure", failed_test_function)
 
-        def error_test_function(self):
-            raise KeyError('grrr')
-        world.error_example = Example("reports error", error_test_function)
+            def error_test_function(self):
+                raise KeyError('grrr')
+            w.error_example = Example("reports error", error_test_function)
 
-    @it('can summarise failures')
-    def spec(world):
-        result = world.failing_example.run(MockExampleGroup(), mock_world_factory)
-        io = StringIO()
-        pf = ProgressFormatter(io)
-        pf.summarise_failures([(world.failing_example, result)])
-        assert io.getvalue() != '' # there must be a better test than this which isn't terrifyingly brittle
+        @it('can summarise failures')
+        def s(w):
+            result = w.failing_example.run(MockExampleGroup(), mock_world_factory)
+            io = StringIO()
+            pf = ProgressFormatter(io)
+            pf.summarise_failures([(w.failing_example, result)])
+            assert io.getvalue() != '' # there must be a better test than this which isn't terrifyingly brittle
 
-    @it('can summarise errors')
-    def spec(world):
-        result = world.failing_example.run(MockExampleGroup(), mock_world_factory)
-        io = StringIO()
-        pf = ProgressFormatter(io)
-        pf.summarise_errors([(world.error_example, result)])
-        assert io.getvalue() != '' # there must be a better test than this which isn't terrifyingly brittle
+        @it('can summarise errors')
+        def s(w):
+            result = w.failing_example.run(MockExampleGroup(), mock_world_factory)
+            io = StringIO()
+            pf = ProgressFormatter(io)
+            pf.summarise_errors([(w.error_example, result)])
+            assert io.getvalue() != '' # there must be a better test than this which isn't terrifyingly brittle
